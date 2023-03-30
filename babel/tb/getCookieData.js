5fa3ba2aee45294200cf9396b2e3a22bbf0e118c
feidepianzi <110144095+feidepianzi@users.noreply.github.com>
2023-03-31 05:38:59 +0800

    Update getCookieData.js

--- babel/tb/getCookieData.js
+++ babel/tb/getCookieData.js
@@ -1,16 +1,6 @@
 // const cookieStr = ``
 // const activityId = ``
 
-function handleCookie () {
-  const Cookie = cookieStr.replace(/\s/g, '')
-  try {
-    const tk = Cookie.match(/_m_h5_tk=(\w+)/)[1]
-    const url = `https://service-daubfate-1251309300.gz.apigw.tencentcs.com/release/api?activityId=${activityId}&tk=${tk}`
-    document.write(JSON.stringify({ Cookie, url }))
-  } catch (error) {
-    document.write(JSON.stringify({ error: `Cookie错误，请检查！` }))
-  }
-}
 
 function handeleStreamCookie () {
   const cookie = cookieStr.match(/Cookie: (.+)/)[1]
